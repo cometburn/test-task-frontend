ba8134d8c57e6dcfe38eb8f067492168a8da80b5
Jc Cometa <cometburn@gmail.com>
2021-10-26 06:11:18 +0800

    Edit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
-var url = document.URL.split(":");
-var port = parseInt(url[2].slice(0, -1)) + 1;
-const apiUrl = `${url[0]}://${url[1].slice(2)}:${port}/api/`;
+// var url = document.URL.split(":");
+// var port = parseInt(url[2].slice(0, -1)) + 1;
+// const apiUrl = `${url[0]}://${url[1].slice(2)}:${port}/api/`;
+
+const apiUrl = process.env.VUE_APP_API_URL;
 
 module.exports = {
   apiUrl,
